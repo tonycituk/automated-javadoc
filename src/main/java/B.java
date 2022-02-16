ac0ef755deb73c645a763bc72f5fbf2cde72581e
tonycituk <tonycituk@gmail.com>
2022-02-16 08:15:55 -0600

    Added example classes

---
+++ src/main/java/B.java
@@ -0,0 +1,19 @@
+package main.java;
+
+
+/**
+ * B is a class that extends from A class
+ * 
+ * @author tonycituk
+ * 
+ */
+public class B extends A {
+
+    /**
+     * This is the unique constructor.
+     */
+    public B(int intValue, String stringValue){
+        super(intValue, stringValue);
+    }
+
+}
